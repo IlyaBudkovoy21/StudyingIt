a5a572b3d400c81fb4f360d9a62e65fa4aa5d400
Илья Будковой <ilyaayli@yandex.ru>
2024-09-25 17:48:25 +0300

    feat: Add models

--- StudyingIt/listTasks/models.py
+++ StudyingIt/listTasks/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Types(models.Model):
+    typeTask = models.CharField(max_length=30)
+
+
+class Tasks(models.Model):
+    name = models.CharField(max_length=50)
+    desc = models.TextField(max_length=1000)
+    type_id = models.ForeignKey(Types, on_delete=models.PROTECT)
