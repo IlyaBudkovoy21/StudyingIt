87d5f32f4f828444ad0a3faa6f7e178fbe479530
Илья Будковой <ilyaayli@yandex.ru>
2024-10-11 10:13:07 +0300

    feat: Change Login View

--- StudyingIt/PersonalAccount/urls.py
+++ StudyingIt/PersonalAccount/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
 from . import views
 
 urlpatterns = [
     path('registration/', views.Registration.as_view()),
-    path('login/', views.Login.as_view()),
+    path('login/', TokenObtainPairView.as_view()),
     path("logout/", views.Logout.as_view())
 ]
--- StudyingIt/PersonalAccount/views.py
+++ StudyingIt/PersonalAccount/views.py
@@ -25,34 +25,6 @@ class Registration(APIView):
         return Response(status=status.HTTP_302_FOUND)
 
 
-class Login(APIView):
-    def post(self, request):
-        data = request.data
-        username = data.get("username", None)
-        password = data.get("password", None)
-        if username is None or password is None:
-            return Response(
-                {"error": "Нужен логин и пароль"},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        user = authenticate(username=username, password=password)
-        if user is None:
-            return Response(
-                {"error": "Такой пользователь не существует"},
-                status=status.HTTP_401_UNAUTHORIZED
-            )
-        refresh_token = RefreshToken.for_user(user)
-        refresh_token.payload.update({
-            "user_id": user.id,
-            "username": user.username
-        }
-        )
-        return Response({
-            "refresh": str(refresh_token),
-            "access": str(refresh_token.access_token)
-        }, status=status.HTTP_200_OK)
-
-
 class Logout(APIView):
     permission_classes = [IsAuthenticated]
 
