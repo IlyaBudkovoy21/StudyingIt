a337cb7ccf699b27badf582a021a9d0aab9b7a63
Илья Будковой <ilyaayli@yandex.ru>
2024-11-12 10:09:45 +0300

    test: Create test db(now it works) and first test to the db

--- StudyingIt/tests/listTasks/views/test_views.py
+++ StudyingIt/tests/listTasks/views/test_views.py
@@ -1,11 +1,21 @@
 import pytest
 from listTasks.views import ListTasksByCat
-from listTasks.models import Tasks
+from listTasks.models import Tasks, Types, CodePatterns
 from hashlib import sha224
 
 
-@pytest.mark.django_db
 class TestListTasksViews:
-    def test_list_by_cat(self):
-        a = Tasks.objects.get(id=1)
-        assert a.hash_name == sha224(a.name.encode()).hexdigest()[:9]
+    @pytest.mark.django_db(databases=["default", "test"])
+    def test_list_by_cat(self, db):
+        t = Types.objects.using("test").create(catTask="Programming")
+        pat = CodePatterns.objects.using("test").create(python="sdaf", cpp="asdf", go="kadsf")
+        check = Tasks.objects.using("test").create(
+            name="Sum of two numbers",
+            desc="Desc sum of two numbers",
+            cat=t,
+            patterns=pat,
+            first_test="asdf",
+            second_test="asdf",
+            cost=100
+        )
+        assert check.hash_name == sha224(check.name.encode()).hexdigest()[:9]
