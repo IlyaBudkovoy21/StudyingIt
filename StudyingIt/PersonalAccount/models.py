65465cceaecf20069ad4bc1035863b3705f4f546
Илья Будковой <ilyaayli@yandex.ru>
2025-01-10 20:28:08 +0300

    feat: Add model for user dates

--- StudyingIt/PersonalAccount/models.py
+++ StudyingIt/PersonalAccount/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class CustomUser(User):
+    count_tasks_easy = models.IntegerField(default=0)
+    count_tasks_medium = models.IntegerField(default=0)
+    count_tasks_hard = models.IntegerField(default=0)
+
+
+class DatesInfoUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True, related_name="date_info")
+    registration_date = models.DateField(auto_now_add=True)
+    day_start_row = models.DateField(null=True)
+    days_in_row = models.IntegerField(default=0)
+
+    class Meta:
+        db_table = "user_dates"
+        verbose_name = "Date information"
+        verbose_name_plural = "Dates information"
